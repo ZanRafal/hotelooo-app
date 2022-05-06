f153bd561e9864e601efab9b1af7efe59e699a96
ZanRafal <01159225@pw.edu.pl>
2022-05-06 22:26:14 +0200

    Added Address.java class to objects

---
+++ src/main/java/model/objects/Address.java
@@ -0,0 +1,40 @@
+package model.objects;
+
+public class Address {
+    private String country;
+    private String city;
+    private String postalCode;
+    private String streetName;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+}
