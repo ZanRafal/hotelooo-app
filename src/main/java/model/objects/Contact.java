9b243271a36aaa610a128846c7bcac40c9ae7089
ZanRafal <01159225@pw.edu.pl>
2022-05-07 01:00:21 +0200

    Zaimplementowano wzorzec Builder do Contact.java

--- src/main/java/model/objects/Contact.java
+++ src/main/java/model/objects/Contact.java
@@ -1,60 +1,79 @@
 package model.objects;
 
 public class Contact {
-    private String phoneNumber;
-    private String fax;
-    private String email;
-    private String nip;
-    private String regon;
+    private final String phoneNumber;
+    private final String fax;
+    private final String email;
+    private final String nip;
+    private final String regon;
+
+    private Contact(ContactBuilder contactBuilder) {
+        this.phoneNumber = contactBuilder.phoneNumber;
+        this.fax = contactBuilder.fax;
+        this.email = contactBuilder.email;
+        this.nip = contactBuilder.nip;
+        this.regon = contactBuilder.regon;
+    }
 
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public String getFax() {
         return fax;
     }
 
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getNip() {
         return nip;
     }
 
-    public void setNip(String nip) {
-        this.nip = nip;
-    }
-
     public String getRegon() {
         return regon;
     }
 
-    public void setRegon(String regon) {
-        this.regon = regon;
+    public static class ContactBuilder {
+        private String phoneNumber;
+        private String fax;
+        private String email;
+        private String nip;
+        private String regon;
+
+
+        public ContactBuilder() {}
+
+        public ContactBuilder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public ContactBuilder setFax(String fax) {
+            this.fax = fax;
+            return this;
+        }
+
+        public ContactBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public ContactBuilder setNip(String nip) {
+            this.nip = nip;
+            return this;
+        }
+
+        public ContactBuilder setRegon(String regon) {
+            this.regon = regon;
+            return this;
+        }
+
+        public Contact build() {
+            return new Contact(this);
+        }
     }
 
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "phoneNumber='" + phoneNumber + '\'' +
-                ", fax='" + fax + '\'' +
-                ", email='" + email + '\'' +
-                ", nip='" + nip + '\'' +
-                ", regon='" + regon + '\'' +
-                '}';
-    }
+
 }
