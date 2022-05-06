0e0a299eb56df591df6f343dc72094925532a2d3
ZanRafal <01159225@pw.edu.pl>
2022-05-06 20:47:36 +0200

    Added Contact.java class to objects

---
+++ src/main/java/model/objects/Contact.java
@@ -0,0 +1,31 @@
+package model.objects;
+
+public class Contact {
+    private String phoneNumber;
+    private String fax;
+    private String email;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
