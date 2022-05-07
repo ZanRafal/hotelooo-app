bee2c7712073e789a5128e5a9abf1618579e7d0a
ZanRafal <01159225@pw.edu.pl>
2022-05-07 16:38:42 +0200

    Contact class implemented with lombok

--- src/main/java/model/objects/Contact.java
+++ src/main/java/model/objects/Contact.java
@@ -1,93 +1,11 @@
 package model.objects;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Contact {
     private String phoneNumber;
-    private String fax;
     private String email;
-    private String nip;
-    private String regon;
-
-    private Contact(ContactBuilder contactBuilder) {
-        this.phoneNumber = contactBuilder.phoneNumber;
-        this.fax = contactBuilder.fax;
-        this.email = contactBuilder.email;
-        this.nip = contactBuilder.nip;
-        this.regon = contactBuilder.regon;
-    }
-
-    public Contact() {
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public String getFax() {
-        return fax;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getNip() {
-        return nip;
-    }
-
-    public String getRegon() {
-        return regon;
-    }
-
-    @Override
-    public String toString() {
-        return "Contact{" +
-                "phoneNumber='" + phoneNumber + '\'' +
-                ", fax='" + fax + '\'' +
-                ", email='" + email + '\'' +
-                ", nip='" + nip + '\'' +
-                ", regon='" + regon + '\'' +
-                '}';
-    }
-
-    public static class ContactBuilder {
-        private String phoneNumber;
-        private String fax;
-        private String email;
-        private String nip;
-        private String regon;
-
-
-        public ContactBuilder() {}
-
-        public ContactBuilder setPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-            return this;
-        }
-
-        public ContactBuilder setFax(String fax) {
-            this.fax = fax;
-            return this;
-        }
-
-        public ContactBuilder setEmail(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public ContactBuilder setNip(String nip) {
-            this.nip = nip;
-            return this;
-        }
-
-        public ContactBuilder setRegon(String regon) {
-            this.regon = regon;
-            return this;
-        }
-
-        public Contact build() {
-            return new Contact(this);
-        }
-    }
-
-
 }
