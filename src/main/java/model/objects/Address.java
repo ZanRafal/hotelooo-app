7ae4dabfe23a1e562fd9bf8ebcac291e8e30e375
ZanRafal <01159225@pw.edu.pl>
2022-05-07 16:51:53 +0200

    implemented lombok in Address class

--- src/main/java/model/objects/Address.java
+++ src/main/java/model/objects/Address.java
@@ -1,77 +1,13 @@
 package model.objects;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Address {
     private String country;
     private String city;
     private String postalCode;
     private String streetName;
-
-    private Address(AddressBuilder addressBuilder) {
-        this.country = addressBuilder.country;
-        this.city = addressBuilder.city;
-        this.postalCode = addressBuilder.postalCode;
-        this.streetName = addressBuilder.streetName;
-    }
-
-    public Address() {
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public String getStreetName() {
-        return streetName;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "country='" + country + '\'' +
-                ", city='" + city + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", streetName='" + streetName + '\'' +
-                '}';
-    }
-
-    public static class AddressBuilder {
-        private String country;
-        private String city;
-        private String postalCode;
-        private String streetName;
-
-        public AddressBuilder() {}
-
-        public AddressBuilder setCountry(String country) {
-            this.country = country;
-            return this;
-        }
-
-        public AddressBuilder setCity(String city) {
-            this.city = city;
-            return this;
-        }
-
-        public AddressBuilder setPostalCode(String postalCode) {
-            this.postalCode = postalCode;
-            return this;
-        }
-
-        public AddressBuilder setStreetName(String streetName) {
-            this.streetName = streetName;
-            return this;
-        }
-
-        public Address build() {
-            return new Address(this);
-        }
-    }
 }
