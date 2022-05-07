ac036c58802448de98035533dcc75d667ffa76e7
ZanRafal <01159225@pw.edu.pl>
2022-05-07 11:46:42 +0200

    Implemented builder design pattern to HotelFacilities.java class

--- src/main/java/model/objects/HotelFacilities.java
+++ src/main/java/model/objects/HotelFacilities.java
@@ -1,59 +1,56 @@
 package model.objects;
 
 public class HotelFacilities extends AbstractBuilding{
-    private Integer numberOfRooms;
-    private Integer numberOfBeds;
-    private Boolean isMaritalBed;
-    private Boolean isTelevision;
-    private Boolean isBathroomInside;
-    private Boolean isKitchenInside;
+    private final Integer numberOfRooms;
+    private final Integer numberOfBeds;
+    private final Boolean isMaritalBed;
+    private final Boolean isTelevision;
+    private final Boolean isBathroomInside;
+    private final Boolean isKitchenInside;
+    private final Boolean isPoolAvailable;
+    private final Boolean isParkingLotAvailable;
+
+    private HotelFacilities(HotelFacilitiesBuilder hotelFacilitiesBuilder) {
+        this.numberOfRooms = hotelFacilitiesBuilder.numberOfRooms;
+        this.numberOfBeds = hotelFacilitiesBuilder.numberOfBeds;
+        this.isMaritalBed = hotelFacilitiesBuilder.isMaritalBed;
+        this.isTelevision = hotelFacilitiesBuilder.isTelevision;
+        this.isBathroomInside = hotelFacilitiesBuilder.isBathroomInside;
+        this.isKitchenInside = hotelFacilitiesBuilder.isKitchenInside;
+        this.isPoolAvailable = hotelFacilitiesBuilder.isPoolAvailable;
+        this.isParkingLotAvailable = hotelFacilitiesBuilder.isParkingLotAvailable;
+    }
 
     public Integer getNumberOfRooms() {
         return numberOfRooms;
-    }
-
-    public void setNumberOfRooms(Integer numberOfRooms) {
-        this.numberOfRooms = numberOfRooms;
     }
 
     public Integer getNumberOfBeds() {
         return numberOfBeds;
     }
 
-    public void setNumberOfBeds(Integer numberOfBeds) {
-        this.numberOfBeds = numberOfBeds;
-    }
-
     public Boolean getMaritalBed() {
         return isMaritalBed;
-    }
-
-    public void setMaritalBed(Boolean maritalBed) {
-        isMaritalBed = maritalBed;
     }
 
     public Boolean getTelevision() {
         return isTelevision;
     }
 
-    public void setTelevision(Boolean television) {
-        isTelevision = television;
-    }
-
     public Boolean getBathroomInside() {
         return isBathroomInside;
-    }
-
-    public void setBathroomInside(Boolean bathroomInside) {
-        isBathroomInside = bathroomInside;
     }
 
     public Boolean getKitchenInside() {
         return isKitchenInside;
     }
 
-    public void setKitchenInside(Boolean kitchenInside) {
-        isKitchenInside = kitchenInside;
+    public Boolean getPoolAvailable() {
+        return isPoolAvailable;
+    }
+
+    public Boolean getParkingLotAvailable() {
+        return isParkingLotAvailable;
     }
 
     @Override
@@ -65,6 +62,63 @@ public class HotelFacilities extends AbstractBuilding{
                 ", isTelevision=" + isTelevision +
                 ", isBathroomInside=" + isBathroomInside +
                 ", isKitchenInside=" + isKitchenInside +
+                ", isPoolAvailable=" + isPoolAvailable +
+                ", isParkingLotAvailable=" + isParkingLotAvailable +
                 '}';
+    }
+
+    public static class HotelFacilitiesBuilder {
+        private Integer numberOfRooms;
+        private Integer numberOfBeds;
+        private Boolean isMaritalBed;
+        private Boolean isTelevision;
+        private Boolean isBathroomInside;
+        private Boolean isKitchenInside;
+        private Boolean isPoolAvailable;
+        private Boolean isParkingLotAvailable;
+
+        public HotelFacilitiesBuilder setNumberOfRooms(Integer numberOfRooms) {
+            this.numberOfRooms = numberOfRooms;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setNumberOfBeds(Integer numberOfBeds) {
+            this.numberOfBeds = numberOfBeds;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setMaritalBed(Boolean maritalBed) {
+            isMaritalBed = maritalBed;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setTelevision(Boolean television) {
+            isTelevision = television;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setBathroomInside(Boolean bathroomInside) {
+            isBathroomInside = bathroomInside;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setKitchenInside(Boolean kitchenInside) {
+            isKitchenInside = kitchenInside;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setPoolAvailable(Boolean poolAvailable) {
+            isPoolAvailable = poolAvailable;
+            return this;
+        }
+
+        public HotelFacilitiesBuilder setParkingLotAvailable(Boolean parkingLotAvailable) {
+            isParkingLotAvailable = parkingLotAvailable;
+            return this;
+        }
+
+        public HotelFacilities build() {
+            return new HotelFacilities(this);
+        }
     }
 }
