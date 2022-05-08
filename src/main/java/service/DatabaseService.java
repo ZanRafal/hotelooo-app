77445d2b7f572036ab27d0dccf79120378b8d643
ZanRafal <01159225@pw.edu.pl>
2022-05-08 23:27:23 +0200

    Added DatabaseService.java interface

---
+++ src/main/java/service/DatabaseService.java
@@ -0,0 +1,17 @@
+package service;
+
+import model.objects.Hotel;
+
+import java.util.List;
+
+public interface DatabaseService {
+
+    List<Hotel> getAllHotels();
+
+    Hotel getHotelByName(String hotelName);
+
+    Hotel getHotelByLocation(String hotelLocation);
+
+    void updateHotelVacancy(Hotel hotel);
+
+}
