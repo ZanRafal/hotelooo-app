18b9d7e60c678b7efac69f16530a6e2d838c1e5f
ZanRafal <01159225@pw.edu.pl>
2022-05-08 23:16:11 +0200

    Thread-safe singleton implementation of JsonObjectReader.java class

--- src/main/java/util/JsonObjectReader.java
+++ src/main/java/util/JsonObjectReader.java
@@ -8,12 +8,30 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class JsonObjectReader extends JsonObjectParser {
+    private static volatile JsonObjectReader INSTANCE;
+
+    private JsonObjectReader() {}
+
+    public static JsonObjectReader getInstance() {
+        JsonObjectReader result = INSTANCE;
+
+        if(result != null) {
+            return result;
+        }
+
+        synchronized (JsonObjectReader.class) {
+            if(INSTANCE == null) {
+                INSTANCE = new JsonObjectReader();
+            }
+        }
+        return INSTANCE;
+    }
 
     public static ArrayList<Hotel> readJsonObjects() {
         ArrayList<Hotel> objects = new ArrayList<>();
         try {
             ObjectMapper objectMapper = new ObjectMapper();
-            objects = objectMapper.readValue(new File(PATH), new TypeReference<ArrayList<Hotel>>(){});
+            objects = objectMapper.readValue(new File(PATH), new TypeReference<>(){});
         } catch (Exception exception) {
             exception.printStackTrace();
         }
