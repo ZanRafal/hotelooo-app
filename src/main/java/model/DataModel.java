fafd3dff0f97cb223e4baa4a949718222eda3a46
ZanRafal <01159225@pw.edu.pl>
2022-05-11 00:28:36 +0200

    Added fields of DataModel.java class

--- src/main/java/model/DataModel.java
+++ src/main/java/model/DataModel.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.Getter;
+import lombok.Setter;
+import model.objects.Hotel;
+
+import java.util.List;
+
+
+@Getter
+@Setter
 public class DataModel {
+    private List<Hotel> hotels;
+    private Hotel activeHotel;
 }
