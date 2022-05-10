6dbafb6aff57352b155513a129db610e7a7b2b07
ZanRafal <01159225@pw.edu.pl>
2022-05-10 23:52:21 +0200

    Created new HotelRoom.java class with its logic

---
+++ src/main/java/model/objects/HotelRoom.java
@@ -0,0 +1,21 @@
+package model.objects;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class HotelRoom {
+    private Integer numberOfBeds;
+    private Boolean isMaritalBed;
+    private Boolean isTelevision;
+    private Boolean isBathroomInside;
+    private Boolean isKitchenInside;
+    private Client client;
+    //TODO zrobić to jako enum
+    private Boolean isOccupied;
+
+    public void toggleOccupied() {
+        this.isOccupied = !isOccupied;
+    }
+}
