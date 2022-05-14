a73c74d51994aa91b5efcfdb816ac8bc9126bc4a
ZanRafal <01159225@pw.edu.pl>
2022-05-14 19:39:39 +0200

    Added skeleton of MainHotelModel.java class

---
+++ src/main/java/model/MainHotelModel.java
@@ -0,0 +1,39 @@
+package model;
+
+import model.objects.HotelRoom;
+import service.DatabaseServiceImpl;
+
+public class MainHotelModel implements HotelModel {
+    private final DatabaseServiceImpl hotelService = new DatabaseServiceImpl();
+    private final HotelDataModel hotelDataModel = new HotelDataModel();
+
+    @Override
+    public HotelDataModel getHotelDataModel() {
+        return null;
+    }
+
+    @Override
+    public void loadHotels() {
+
+    }
+
+    @Override
+    public void loadHotelById(int hotelId) {
+
+    }
+
+    @Override
+    public void loadHotelByName(String hotelName) {
+
+    }
+
+    @Override
+    public void loadHotelByLocation(String location) {
+
+    }
+
+    @Override
+    public void changeHotelData(HotelRoom newHotelRoom) {
+
+    }
+}
