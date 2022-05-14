eac8a4d37030552f7b3069d48a9d7fde72f93397
ZanRafal <01159225@pw.edu.pl>
2022-05-14 21:37:57 +0200

    Added basic logic of MainRoomModel

--- src/main/java/model/MainRoomModel.java
+++ src/main/java/model/MainRoomModel.java
@@ -1,10 +1,13 @@
 package model;
 
 import model.objects.Client;
+import model.objects.HotelRoom;
 
+
+//TODO przemyśleć i dokończyć logikę całości
+//TODO IMPORTANT
 public class MainRoomModel implements RoomModel {
     private final RoomDataModel roomDataModel = new RoomDataModel();
-
 
     @Override
     public RoomDataModel getRoomDataModel() {
@@ -13,16 +16,18 @@ public class MainRoomModel implements RoomModel {
 
     @Override
     public void loadHotelRoomById(int roomId) {
-
+        HotelRoom hotelRoom = roomDataModel.getActiveRoom();
+        roomDataModel.setActiveRoom(hotelRoom);
     }
 
     @Override
     public void changeRoomData(Client newClient) {
-
+        HotelRoom hotelRoom = roomDataModel.getActiveRoom();
+        hotelRoom.setClient(newClient);
     }
 
     @Override
     public void changeRoomOccupation() {
-
+        roomDataModel.getActiveRoom().toggleOccupied();
     }
 }
