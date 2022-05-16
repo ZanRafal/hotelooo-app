14e53a6f442ff13ea16dd12ea9ce17337f6529ba
ZanRafal <01159225@pw.edu.pl>
2022-05-16 21:32:49 +0200

    Deleted unnecessary imports

--- src/main/java/model/objects/AbstractBuilding.java
+++ src/main/java/model/objects/AbstractBuilding.java
@@ -1,10 +1,5 @@
 package model.objects;
 
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
 public abstract class AbstractBuilding {
     private Integer id;
     private String name;
