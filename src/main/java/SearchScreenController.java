93ac098cbcc1355357f73411b34a689e3d74038b
ZanRafal <01159225@pw.edu.pl>
2022-05-16 21:49:10 +0200

    Added SearchScreenController.java class

---
+++ src/main/java/SearchScreenController.java
@@ -0,0 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class SearchScreenController {
+
+    @FXML
+    private TextField city_search_bar;
+
+    @FXML
+    private Pane main_frame;
+
+    @FXML
+    private Button search_button;
+
+}
