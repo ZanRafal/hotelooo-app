480d702bd2cb4fd44512359b9f05a88bc5073ada
ZanRafal <01159225@pw.edu.pl>
2022-05-17 18:50:22 +0200

    Updated fields of HotelFacilities.java

--- src/main/java/model/objects/HotelFacilities.java
+++ src/main/java/model/objects/HotelFacilities.java
@@ -1,14 +1,23 @@
 package model.objects;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class HotelFacilities {
     private Integer stars;
     private Integer numberOfRooms;
     private Boolean isPool;
     private Boolean isParkingLot;
     private Boolean isSpa;
+    private Boolean isMaritalBed;
+    private Boolean isTelevision;
+    private Boolean isBathroomInside;
+    private Boolean isKitchenInside;
+
+    public HotelFacilities() {
+    }
 }
