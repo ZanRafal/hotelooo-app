1c38f35fbec675aa13c6a77842fb0b0fa25bb8ac
ZanRafal <01159225@pw.edu.pl>
2022-05-17 18:21:19 +0200

    Added required constructors

--- src/main/java/model/objects/Contact.java
+++ src/main/java/model/objects/Contact.java
@@ -1,11 +1,17 @@
 package model.objects;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Contact {
     private String phoneNumber;
     private String email;
+
+    public Contact() {
+    }
 }
