4113d312124a64e9330923d5adec80f814e425a1
ZanRafal <01159225@pw.edu.pl>
2022-05-17 18:21:13 +0200

    Added required constructors

--- src/main/java/model/objects/Address.java
+++ src/main/java/model/objects/Address.java
@@ -1,13 +1,16 @@
 package model.objects;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Address {
     private String country;
     private String city;
     private String postalCode;
     private String streetName;
+
+    public Address() {
+    }
 }
