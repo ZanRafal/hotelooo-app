e1064dfeed976c26c4d9ed728e96dffb28b57a20
ZanRafal <01159225@pw.edu.pl>
2022-05-17 21:02:47 +0200

    Added relative Path field

--- src/main/java/util/JsonObjectParser.java
+++ src/main/java/util/JsonObjectParser.java
@@ -1,5 +1,8 @@
 package util;
 
+import java.io.File;
+import java.util.Objects;
+
 public abstract class JsonObjectParser {
-    protected static final String PATH = "src/hotels.json";
+        protected static final File FILE = new File(Objects.requireNonNull(JsonObjectParser.class.getClassLoader().getResource("hotele.json")).getPath());
 }
