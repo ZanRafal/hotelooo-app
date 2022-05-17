402514ab951189775b5c70dc1901c64f1cf5b76d
ZanRafal <01159225@pw.edu.pl>
2022-05-17 18:21:05 +0200

    Added required constructors

--- src/main/java/model/objects/Client.java
+++ src/main/java/model/objects/Client.java
@@ -1,12 +1,18 @@
 package model.objects;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class Client {
     private String firstName;
     private String lastName;
     private Contact contact;
+
+    public Client() {
+    }
 }
