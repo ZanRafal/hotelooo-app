98d7fb5902f949448ff9b728283c8d2f24357e58
ZanRafal <01159225@pw.edu.pl>
2022-05-17 20:51:57 +0200

    Added ScreensController.java class

--- src/main/java/controller/HotelController.java
+++ src/main/java/controller/HotelController.java
@@ -1,17 +1,25 @@
 package controller;
 
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import model.HotelModel;
+import model.MainHotelModel;
 import view.EditRoomView;
 import view.HotelView;
 
 @Setter
 public class HotelController {
-    private HotelModel hotelModel;
+    private HotelModel hotelModel = new MainHotelModel();
     private HotelView hotelView;
     private EditRoomView editRoomView;
 
     public void onLoadAllUsers() {
         hotelModel.loadHotels();
+        System.out.println(hotelModel.getHotelDataModel().getHotels());
+    }
+
+    public void onLoadAllHotelsFromMatchingCity(String cityName) {
+        hotelModel.loadAllHotelsFromMatchingCity(cityName);
+        System.out.println(hotelModel.getHotelDataModel().getHotels());
     }
 }
