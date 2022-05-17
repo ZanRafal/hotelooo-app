6f6dfad463782a54d01b80d5c30b2b57eb101872
ZanRafal <01159225@pw.edu.pl>
2022-05-17 21:50:16 +0200

    Updated JsonObjectParser.java logic

--- src/main/java/util/JsonObjectParser.java
+++ src/main/java/util/JsonObjectParser.java
@@ -3,6 +3,9 @@ package util;
 import java.io.File;
 import java.util.Objects;
 
+import java.io.File;
+import java.util.Objects;
+
 public abstract class JsonObjectParser {
         protected static final File FILE = new File(Objects.requireNonNull(JsonObjectParser.class.getClassLoader().getResource("hotele.json")).getPath());
 }
