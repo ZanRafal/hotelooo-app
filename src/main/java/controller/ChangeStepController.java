9b2c93c4dca906c620740f6d70fb41bbb351d6bb
ZanRafal <01159225@pw.edu.pl>
2022-05-17 21:24:24 +0200

    Made ChangeStepController as descendant of StackPane, added necessary field and constructor

--- src/main/java/controller/ChangeStepController.java
+++ src/main/java/controller/ChangeStepController.java
@@ -1,4 +1,21 @@
 package controller;
 
-public class ChangeStepController {
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Setter
+public class ChangeStepController extends StackPane {
+
+    private Map<String, Node> screens = new HashMap<>();
+
+    public ChangeStepController() {
+        super();
+    }
+
 }
