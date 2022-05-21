48b3a3baac7dd3f0173d4afc09c5cb06f713103a
ZanRafal <01159225@pw.edu.pl>
2022-05-22 01:23:12 +0200

    Moved constants to ScreenUtils.java

--- src/main/java/controller/ScreensController.java
+++ src/main/java/controller/ScreensController.java
@@ -4,28 +4,19 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import util.ScreenUtils;
 
 public class ScreensController extends Application {
-    //TODO przenieść to do Utility
-    public static String SEARCH_SCREEN = "/screens/hotel_search_screen.fxml";
-    public static String SEARCH_SCREEN_ID = "search_screen";
-    public static String HOTEL_LIST_SCREEN = "/screens/hotels_list_screen.fxml";
-    public static String HOTEL_LIST_SCREEN_ID = "hotels_list";
-    public static String HOTEL_DETAILS_SCREEN = "screens/hotel_details_screen.fxml";
-    public static String HOTEL_DETAILS_SCREEN_ID = "hotel_details";
-    public static String PARENT_SCREEN = "screens/MainAppContainter.fxml";
-    public static String PARENT_SCREEN_ID = "parent_screen";
-
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         ChangeStepController mainContainer = new ChangeStepController();
 
-        mainContainer.loadScreen(ScreensController.SEARCH_SCREEN_ID, ScreensController.SEARCH_SCREEN);
-//        mainContainer.loadScreen(ScreensController.HOTEL_LIST_SCREEN_ID, ScreensController.HOTEL_LIST_SCREEN);
-        //mainContainer.loadScreen(ScreensController.HOTEL_DETAILS_SCREEN_ID, ScreensController.HOTEL_DETAILS_SCREEN);
+        mainContainer.loadScreen(ScreenUtils.SEARCH_SCREEN_ID, ScreenUtils.SEARCH_SCREEN);
+        mainContainer.loadScreen(ScreenUtils.HOTEL_LIST_SCREEN_ID, ScreenUtils.HOTEL_LIST_SCREEN);
+        mainContainer.loadScreen(ScreenUtils.HOTEL_DETAILS_SCREEN_ID, ScreenUtils.HOTEL_DETAILS_SCREEN);
 
-        mainContainer.setScreen(ScreensController.SEARCH_SCREEN_ID);
+        mainContainer.setScreen(ScreenUtils.SEARCH_SCREEN_ID);
 
         Group root = new Group();
         root.getChildren().addAll(mainContainer);
