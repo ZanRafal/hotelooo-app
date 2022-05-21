8e2acbb143ad298c20cf4452b79da1ad6e79f6e6
ZanRafal <01159225@pw.edu.pl>
2022-05-21 17:10:02 +0200

    Bugfix

--- src/main/java/controller/ScreenController.java
+++ src/main/java/controller/ScreenController.java
@@ -1,7 +1,8 @@
 package controller;
 
 //interfejs odpowiadający za ustawianie parenta dla danego ekranu
+//controlled screen
 public interface ScreenController {
 
-    void setScreenParent(ScreensController screenParent);
+    void setScreenParent(ChangeStepController screenParent);
 }
