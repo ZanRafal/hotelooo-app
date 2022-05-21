8cbf0ec138f1b508d6baa5f83a11ae5c32ab8026
ZanRafal <01159225@pw.edu.pl>
2022-05-21 23:35:51 +0200

    Bugfixes and added necessary methods

--- src/main/java/controller/ScreensController.java
+++ src/main/java/controller/ScreensController.java
@@ -7,30 +7,32 @@ import javafx.stage.Stage;
 
 public class ScreensController extends Application {
     //TODO przenieść to do Utility
-    private static final String SEARCH_SCREEN = "SearchPageScreen.fxml";
-    private static final String SEARCH_SCREEN_ID = "search_screen";
-    private static final String HOTEL_LIST_SCREEN = "HotelsListScreen.fxml";
-    private static final String HOTEL_LIST_SCREEN_ID = "hotels_list";
-    private static final String HOTEL_DETAILS_SCREEN = "HotelDetailsScreen.fxml";
-    private static final String HOTEL_DETAILS_SCREEN_ID = "hotel_details";
-    private static final String PARENT_SCREEN = "MainAppContainter.fxml";
-    private static final String PARENT_SCREEN_ID = "parent_screen";
+    public static String SEARCH_SCREEN = "/screens/hotel_search_screen.fxml";
+    public static String SEARCH_SCREEN_ID = "search_screen";
+    public static String HOTEL_LIST_SCREEN = "/screens/hotels_list_screen.fxml";
+    public static String HOTEL_LIST_SCREEN_ID = "hotels_list";
+    public static String HOTEL_DETAILS_SCREEN = "screens/hotel_details_screen.fxml";
+    public static String HOTEL_DETAILS_SCREEN_ID = "hotel_details";
+    public static String PARENT_SCREEN = "screens/MainAppContainter.fxml";
+    public static String PARENT_SCREEN_ID = "parent_screen";
 
 
     @Override
     public void start(Stage stage) throws Exception {
         ChangeStepController mainContainer = new ChangeStepController();
 
-        mainContainer.loadScreen(SEARCH_SCREEN, SEARCH_SCREEN_ID);
-        mainContainer.loadScreen(HOTEL_LIST_SCREEN, HOTEL_LIST_SCREEN_ID);
-        mainContainer.loadScreen(HOTEL_DETAILS_SCREEN, HOTEL_DETAILS_SCREEN_ID);
+        mainContainer.loadScreen(ScreensController.SEARCH_SCREEN_ID, ScreensController.SEARCH_SCREEN);
+//        mainContainer.loadScreen(ScreensController.HOTEL_LIST_SCREEN_ID, ScreensController.HOTEL_LIST_SCREEN);
+        //mainContainer.loadScreen(ScreensController.HOTEL_DETAILS_SCREEN_ID, ScreensController.HOTEL_DETAILS_SCREEN);
 
-        mainContainer.setScreen(PARENT_SCREEN);
+        mainContainer.setScreen(ScreensController.SEARCH_SCREEN_ID);
 
         Group root = new Group();
         root.getChildren().addAll(mainContainer);
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        //TODO Dodać do propertasów albo utilki
+        stage.setTitle("Hotelooo");
         stage.show();
     }
 
