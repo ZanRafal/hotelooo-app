5544891224bbb857e2ee17d19026c9009641f8fd
ZanRafal <01159225@pw.edu.pl>
2022-05-21 17:18:19 +0200

    Added necessary fields

--- src/main/java/controller/ScreensController.java
+++ src/main/java/controller/ScreensController.java
@@ -1,13 +1,15 @@
 package controller;
 
-import javafx.application.Application;
-
 public class ScreensController {
+    //TODO przenieść to do Utility
     private static final String SEARCH_SCREEN = "SearchPageScreen.fxml";
     private static final String SEARCH_SCREEN_ID = "search_screen";
     private static final String HOTEL_LIST_SCREEN = "HotelsListScreen.fxml";
     private static final String HOTEL_LIST_SCREEN_ID = "hotels_list";
     private static final String HOTEL_DETAILS_SCREEN = "HotelDetailsScreen.fxml";
     private static final String HOTEL_DETAILS_SCREEN_ID = "hotel_details";
+    private static final String PARENT_SCREEN = "MainAppContainter.fxml";
+    private static final String PARENT_SCREEN_ID = "parent_screen";
+
 
 }
