277e46235c0a1e26ac515355eb9db5cb3b54b82e
ZanRafal <01159225@pw.edu.pl>
2022-05-21 17:19:38 +0200

    Added loadScreen and setScreen methods to ChangeStepController

--- src/main/java/controller/ChangeStepController.java
+++ src/main/java/controller/ChangeStepController.java
@@ -1,15 +1,19 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
 @Setter
+@Slf4j
 public class ChangeStepController extends StackPane {
 
     private Map<String, Node> screens = new HashMap<>();
@@ -26,4 +30,31 @@ public class ChangeStepController extends StackPane {
         return screens.get(name);
     }
 
+    public boolean loadScreen(String name, String resource) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
+            Parent loadScreen = (Parent) loader.load();
+            ScreenController controlledScreen = ((ScreenController) loader.getController());
+            controlledScreen.setScreenParent(this);
+            addScreen(name, loadScreen);
+            return true;
+        } catch (Exception exception) {
+            log.error(exception.getMessage());
+            return false;
+        }
+    }
+
+    //TODO dadać custom wyjątek
+    public boolean setScreen(String name) throws Exception {
+        if(name == null) {
+            throw new Exception("Error");
+        }
+
+        if (!getChildren().isEmpty()) {
+            getChildren().add(screens.get(name));
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
