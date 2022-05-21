eaa05a5458f02b8101d6e334b055b889caac1b76
ZanRafal <01159225@pw.edu.pl>
2022-05-21 17:10:49 +0200

    implemented ScreensController interface in SearchScreenController class

--- src/main/java/controller/SearchScreenController.java
+++ src/main/java/controller/SearchScreenController.java
@@ -5,8 +5,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-public class SearchScreenController  {
+public class SearchScreenController implements ScreenController {
     private static final HotelController controller = new HotelController();
+    ChangeStepController changeStepController;
 
     @FXML
     private TextField citySearchBar;
@@ -17,9 +18,16 @@ public class SearchScreenController  {
     @FXML
     private Button searchButton;
 
+
+    @Override
+    public void setScreenParent(ChangeStepController screenParent) {
+        this.changeStepController = screenParent;
+    }
+
     @FXML
     protected void onSearchButtonClicked() {
         controller.onLoadAllHotelsFromMatchingCity(citySearchBar.getText());
+
     }
 
     @FXML
