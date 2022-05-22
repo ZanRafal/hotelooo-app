041c2f4e6683a06c807bf764341e0c2d5ac414e0
ZanRafal <01159225@pw.edu.pl>
2022-05-22 20:48:23 +0200

    Changed file destination path

--- src/main/java/util/JsonObjectParser.java
+++ src/main/java/util/JsonObjectParser.java
@@ -7,5 +7,5 @@ import java.io.File;
 import java.util.Objects;
 
 public abstract class JsonObjectParser {
-        protected static final File FILE = new File(Objects.requireNonNull(JsonObjectParser.class.getClassLoader().getResource("hotele.json")).getPath());
+        protected static final String PATH = "hotele.json";
 }
