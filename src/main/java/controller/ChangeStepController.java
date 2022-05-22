cf2df2f7f178a96165b7349730193d0fb3b387b8
ZanRafal <01159225@pw.edu.pl>
2022-05-22 16:22:50 +0200

    Optimized imports

--- src/main/java/controller/ChangeStepController.java
+++ src/main/java/controller/ChangeStepController.java
@@ -1,16 +1,9 @@
 package controller;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.beans.property.DoubleProperty;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
-import javafx.util.Duration;
 import lombok.Getter;
 import lombok.Setter;
 
