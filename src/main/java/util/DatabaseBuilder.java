9477b9259ad93b291426f692abd45d5724cf9845
ZanRafal <01159225@pw.edu.pl>
2022-05-22 16:51:36 +0200

    Bugfixes in DatabaseBuilder.java

--- src/main/java/util/DatabaseBuilder.java
+++ src/main/java/util/DatabaseBuilder.java
@@ -6,26 +6,26 @@ import model.objects.*;
 import java.util.List;
 
 @UtilityClass
+//@Deprecated
 public class DatabaseBuilder {
     JsonObjectWriter objectWriter = JsonObjectWriter.getInstance();
     JsonObjectReader objectReader = JsonObjectReader.getInstance();
 
     //pamiętać o prawidłowym formacie
     Address address = Address.builder()
-            .city()
-            .country()
-            .postalCode()
-            .streetName()
+            .city("Warszawa")
+            .country("Poland")
+            .postalCode("12-331")
+            .streetName("Ulicowa")
             .build();
 
     //pamiętać o prawidłowym formacie
     HotelContact hotelContact = HotelContact.builder()
-            .email()
-            .email()
-            .fax()
-            .nip()
-            .phoneNumber()
-            .regon()
+            .email("mock123@wp.pl")
+            .fax("fax")
+            .nip("nip")
+            .phoneNumber("123-231-123")
+            .regon("jest")
             .build();
 
     HotelRoom hotelRoom = HotelRoom.builder()
@@ -36,33 +36,32 @@ public class DatabaseBuilder {
 
     //true lub false
     HotelFacilities hotelFacilities = HotelFacilities.builder()
-            .isBathroomInside()
-            .isKitchenInside()
-            .isMaritalBed()
-            .isParkingLot()
-            .isPool()
-            .isSpa()
-            .isTelevision()
-            .numberOfRooms()
-            .stars()
+            .isBathroomInside(true)
+            .isKitchenInside(false)
+            .isMaritalBed(true)
+            .isParkingLot(false)
+            .isPool(true)
+            .isSpa(false)
+            .isTelevision(false)
+            .numberOfRooms(6)
+            .stars(3)
             .build();
 
 
     Hotel hotel = Hotel.builder()
-            .name()
+            .id(1)
+            .name("Zielona dolina")
             .address(address)
             .contact(hotelContact)
             .hotelRoom(hotelRoom)
             .hotelFacilities(hotelFacilities)
-            .price() //float
-            .imgUrl() //string
+            .price(123.3f) //float
+            .imgUrl("") //string
             .build();
 
 
     public static void initializeDatabase() {
         List<Hotel> hotelList = objectReader.readJsonObjects();
-        int id = hotelList.get(hotelList.size() - 1).getId();
-        hotel.setId(id + 1);
         hotelList.add(hotel);
         objectWriter.writeJsonObjects(hotelList);
     }
