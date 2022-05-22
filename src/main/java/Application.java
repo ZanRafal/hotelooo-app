eb6380fc055956d186ad4e00d448ca1be475c910
ZanRafal <01159225@pw.edu.pl>
2022-05-22 13:41:58 +0200

    Pushed Application main

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,9 @@
-public class Application {
+import controller.ApplicationFacade;
+
+public class Application  {
+
     public static void main(String[] args) {
+//        ApplicationFacade.runApplication(args);
 
     }
 }
