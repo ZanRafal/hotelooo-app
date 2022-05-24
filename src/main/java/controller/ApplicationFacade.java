9c678bdc0948dd04f8251305c71e903ded43d7fb
ZanRafal <01159225@pw.edu.pl>
2022-05-25 00:02:10 +0200

    Added ApplicationFacade class

--- src/main/java/controller/ApplicationFacade.java
+++ src/main/java/controller/ApplicationFacade.java
@@ -1,9 +1,14 @@
 package controller;
 
+import controller.frontend.ScreensController;
+import service.ApplicationModelInitializerService;
+
 public class ApplicationFacade {
     private static final ScreensController screensController = new ScreensController();
+    public static final ApplicationModelInitializerService application = ApplicationModelInitializerService.getInstance();
 
     public static void runApplication(String[] args) {
+        application.initialize();
         screensController.run(args);
     }
 }
