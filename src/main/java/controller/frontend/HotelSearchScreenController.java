e9b61c29ccd660ae1c007ede11afb8c8920cac35
ZanRafal <01159225@pw.edu.pl>
2022-05-25 00:19:21 +0200

    Implemented Logic in HotelSearchScreenController

--- src/main/java/controller/frontend/HotelSearchScreenController.java
+++ src/main/java/controller/frontend/HotelSearchScreenController.java
@@ -1,17 +1,20 @@
-package controller;
+package controller.frontend;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import controller.HotelController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import service.ApplicationModelInitializerService;
 import util.ScreenUtils;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class HotelSearchScreenController implements Initializable, ScreenController {
-    private static final HotelController controller = new HotelController();
+    private static final HotelController controller = ApplicationModelInitializerService.getController();
     private ChangeStepController changeStepController;
 
     @FXML
@@ -36,18 +39,16 @@ public class HotelSearchScreenController implements Initializable, ScreenControl
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
     }
 
     @FXML
     void getCitySearchBarInput(ActionEvent event) {
-
     }
 
     @FXML
     void onSearchButtonClicked(ActionEvent event) {
         controller.onLoadAllHotelsFromMatchingCity(citySearchBar.getText());
-        changeStepController.setScreen(ScreenUtils.HOTEL_LIST_SCREEN_ID);
+        nextStep();
     }
 
     @FXML
@@ -55,8 +56,10 @@ public class HotelSearchScreenController implements Initializable, ScreenControl
         assert citySearchBar != null : "fx:id=\"citySearchBar\" was not injected: check your FXML file 'hotel_search_screen.fxml'.";
         assert hotel_search_screen != null : "fx:id=\"hotel_search_screen\" was not injected: check your FXML file 'hotel_search_screen.fxml'.";
         assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'hotel_search_screen.fxml'.";
-
     }
 
-
+    private void nextStep() {
+        changeStepController.loadScreen(ScreenUtils.HOTEL_LIST_SCREEN_ID, ScreenUtils.HOTEL_LIST_SCREEN);
+        changeStepController.setScreen(ScreenUtils.HOTEL_LIST_SCREEN_ID);
+    }
 }
