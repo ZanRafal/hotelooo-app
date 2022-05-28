b58b5d0502024ac0d72f8d7c13e01f0fa8a09139
ZanRafal <01159225@pw.edu.pl>
2022-05-29 00:50:11 +0200

    Added ImgSources utility class for storing image sources

---
+++ src/main/java/util/ImgSources.java
@@ -0,0 +1,14 @@
+package util;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.HashMap;
+
+@UtilityClass
+public class ImgSources {
+
+    public static HashMap<String, String> values = new HashMap<>() {{
+
+    }};
+
+}
