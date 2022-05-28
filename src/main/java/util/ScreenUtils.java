8f07fd0ef0ea7bfcce077b110ce94befe701a04e
ZanRafal <01159225@pw.edu.pl>
2022-05-29 00:52:04 +0200

    Added new screen to ScreenUtils.java

--- src/main/java/util/ScreenUtils.java
+++ src/main/java/util/ScreenUtils.java
@@ -1,6 +1,5 @@
 package util;
 
-import jdk.jfr.Description;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -11,10 +10,12 @@ public final class ScreenUtils {
     public static final String HOTEL_LIST_SCREEN = "/screens/hotels_list_screen.fxml";
     public static final String HOTEL_DETAILS_SCREEN = "/screens/hotel_details_screen.fxml";
     public static final String HOTEL_RESERVATION_SCREEN = "/screens/reservation_screen.fxml";
+    public static final String FINALIZE_TRANSACTION_SCREEN = "/screens/finalize_transaction_screen.fxml";
 
     //Screen ID's
     public static final String SEARCH_SCREEN_ID = "search_screen";
     public static final String HOTEL_LIST_SCREEN_ID = "hotels_list";
     public static final String HOTEL_DETAILS_SCREEN_ID = "hotel_details";
     public static final String HOTEL_RESERVATION_SCREEN_ID = "reservation_screen";
+    public static final String FINALIZE_TRANSACTION_SCREEN_ID = "finalize_transaction_screen";
 }
